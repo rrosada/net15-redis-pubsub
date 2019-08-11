fd3d041615401291e432fadd6864af3a1c5e6445
Home <Home@DESKTOP-L1D8RQA>
2019-08-11 12:44:44 -0300

    Criacao projeto redis-pubsub

---
+++ ConsoleAppRedisPubSub/Program.cs
@@ -0,0 +1,51 @@
+﻿using StackExchange.Redis;
+using System;
+
+
+namespace ConsoleAppRedisPubSub
+{
+    class Program
+    {
+        static string _configuration = "40.77.24.62";
+        static string _channel = "Perguntas";
+        static ConnectionMultiplexer redis = null;
+
+        static void Main(string[] args)
+        {
+            OpenConnection();
+            Listener();
+
+            Console.ReadLine();
+        }
+
+        static void OpenConnection()
+        {            
+            redis = ConnectionMultiplexer.Connect(_configuration);
+        }
+
+        static void Listener()
+        {
+            if (!redis.IsConnected)
+                OpenConnection();
+
+            var sub = redis.GetSubscriber();
+            sub.Subscribe(_channel, (ch, msg) =>
+            {               
+                Response(msg.ToString().Split(':')[0]);
+            });
+        }
+
+        static void Response(string value)
+        {            
+            IDatabase db = redis.GetDatabase();
+
+            HashEntry[] hashEntries =
+            {
+                new HashEntry ("RodrigoMarcos", "Response OK")
+            };
+
+            db.HashSet(value, hashEntries);
+            var sub = redis.GetSubscriber();
+        }
+    }
+}
